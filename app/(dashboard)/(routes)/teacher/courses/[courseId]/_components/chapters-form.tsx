6416f20a4e693b5cba2a4d9bec4e17a6fa854452
jadavkeshav <jadavkeshav2005@gmail.com>
2024-07-07 12:57:57 +0530

    added COR

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -0,0 +1,148 @@
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil, PlusCircle } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+import { Chapter, Course } from "@prisma/client";
+import { ChaptersList } from "./chapters-list";
+
+interface ChaptersFormProps {
+    initialData: Course & { chapters: Chapter[] };
+    courseId: String;
+}
+
+const formSchema = z.object({
+    title: z.string().min(1),
+
+})
+
+export const ChaptersForm = ({
+    initialData,
+    courseId
+}: ChaptersFormProps) => {
+    const [isCreating, setIsCreating] = useState(false);
+    const [isUpdating, setIsUpdating] = useState(false);
+
+    const toggleCreating = () => {
+        setIsCreating((current) => !current)
+    };
+
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+        },
+    });
+
+    const { isSubmitting, isValid } = form.formState;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            await axios.post(`/api/courses/${courseId}/chapters`, values);
+            toast.success("Chapter created");
+            toggleCreating();
+            router.refresh();
+        } catch (error) {
+            toast.error("Something went wrong");
+        }
+    }
+
+
+    const onReorder = async (updateData: { id: String; position: number }[]) => {
+        try {
+            setIsUpdating(false);
+            await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+                list: updateData
+            });
+            toast.success("Chapters reordered");
+            router.refresh();
+        } catch (error) {
+            toast.error("Something went wrong");
+        } finally {
+            setIsUpdating(false);
+        }
+    }
+
+
+    return (
+        <div className="mt-6 border bg-slate-100 rounded-md p-4">
+            <div className="font-medium flex items-center justify-between">
+                Course Chapters
+                <Button onClick={toggleCreating} variant={"ghost"} >
+                    {isCreating ? (
+                        <>Cancel</>
+                    ) : (
+                        <>
+                            <PlusCircle className="h-4 w-4 mr-2" />
+                            Add a chapter
+                        </>
+                    )
+                    }
+
+                </Button>
+            </div>
+
+            {isCreating && (
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+                        <FormField
+                            control={form.control}
+                            name="title"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="e.g. 'Intoduction to The Course'"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button disabled={isSubmitting || !isValid} type="submit">
+                            Create
+                        </Button>
+                    </form>
+                </Form>
+            )}
+            {!isCreating && (
+                <div className={cn("text-xm mt-2", !initialData?.chapters.length && "text-slate-500 italic")}>
+                    {!initialData.chapters.length && "No chapters"}
+                    <ChaptersList 
+                        onEdit={()=>{}}
+                        onReorder={onReorder}
+                        items={initialData.chapters || []}
+                    />
+                </div>
+            )}
+            {
+                !isCreating && (
+                    <p className="text-xs text-muted-foreground mt-4">
+                        Drag and drop to reorder the chapters
+                    </p>
+                )
+            }
+        </div>
+    );
+}
--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -23,9 +24,15 @@ const CourseIdPage = async ({
 
     const course = await db.course.findUnique({
         where: {
-            id: params.courseId
+            id: params.courseId,
+            userId
         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc"
+                },
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc"
@@ -50,7 +57,8 @@ const CourseIdPage = async ({
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished)
     ]
 
 
@@ -110,9 +118,10 @@ const CourseIdPage = async ({
                                     Course Chapters
                                 </h2>
                             </div>
-                            <div>
-                                TODO: CHAPTERS
-                            </div>
+                            <ChaptersForm
+                                initialData={course}
+                                courseId={course.id}
+                            />
                         </div>
                         <div>
                             <div className="flex items-center gap-x-2">
